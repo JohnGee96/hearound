2ee6e1e08fbdfae0edb8dd3e1434152b51f1da00
Maxwell Turpin <Maxwell@Maxwells-MBP-3.fios-router.home>
2017-10-23 11:09:52 -0400

    added mylocation button

--- app/Hearound/app/src/main/java/com/hearound/hearound/MainActivity.java
+++ app/Hearound/app/src/main/java/com/hearound/hearound/MainActivity.java
@@ -1,14 +1,33 @@
 package com.hearound.hearound;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    // Constants
+    private final int DEFAULT_GPS_MIN_TIME = 1; // in milliseconds
+    private final int DEFAULT_GPS_MIN_DISTANCE = 1; // in meters
 
+    private static final int MY_PERMISSIONS_ACCESS_FINE_LOCATION = 0;
     private MapView mapView;
+    private MapboxMap mapboxMap;
+    private LocationManager locManager;
+    private MyLocationListener locListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +36,66 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
+        Log.d("****** onCreate *****", "map: " + mapView);
 
+        // Set up location detection
+        locManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        // Set up my location listener
+        locListener = new MyLocationListener(this);
+
+        /* Check permissions
+           Without this, you will receiveERROR:  Caused by: java.lang.SecurityException: "gps"
+           location provider requires ACCESS_FINE_LOCATION permission.
+           More: http://stackoverflow.com/questions/32083913/android-gps-requires-access-fine-location-error-even-though-my-manifest-file
+           Documentation: https://developer.android.com/training/permissions/requesting.html*/
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_ACCESS_FINE_LOCATION);
+        }
+
+        // Needed so onMapReady gets called
+        mapView.getMapAsync(this);
+        // Add button click listener
+        userLocationFAB();
+    }
+
+
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+        MainActivity.this.mapboxMap = mapboxMap;
+        Log.d("****** onMapReady *****", "map: " + mapboxMap);
+        mapboxMap.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        // Callback on permissions grant or deny screen
+        if (requestCode == MY_PERMISSIONS_ACCESS_FINE_LOCATION
+                && grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // Yay!  Now you can do request for location updates
+            locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    DEFAULT_GPS_MIN_TIME,
+                    DEFAULT_GPS_MIN_DISTANCE,
+                    locListener);
+        }
+    }
+
+    // https://github.com/mapbox/mapbox-gl-native/issues/3365
+    private void userLocationFAB(){
+        FloatingActionButton FAB = (FloatingActionButton) findViewById(R.id.myLocationButton);
+        FAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mapboxMap != null && mapboxMap.getMyLocation() != null) {
+                    mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mapboxMap.getMyLocation()), 15));
+                }
+                else {
+                    Log.d("****** FAB *****", "map: " + mapboxMap);
+                }
+            }
+        });
     }
 
     @Override
